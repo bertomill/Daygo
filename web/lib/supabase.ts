d1c8468f591bdf2850b71d646576f117a81441f9
Berto MIll <81169127+bertomill@users.noreply.github.com>
2025-12-13 17:13:52 -0500

    Fix supabase.ts types import path

--- web/lib/supabase.ts
+++ web/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Import types from shared folder
-import type { Database } from '../types/database'
+import type { Database } from './types/database'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
